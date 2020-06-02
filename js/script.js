0c2cabe9626e1ad98528e59df568fba012e3b947
tsudatakuma <tsuda_tacma@yahoo.co.jp>
2020-06-03 00:22:29 +0900

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,60 @@
+// PhotoSwipe
+initPhotoSwipeFromDOM(".js-my-gallery");
+
+$(function () {
+
+  //iOS対策
+  var ua = navigator.userAgent;
+  if (/iPhone|iPad|iPod/.test(ua)) {
+    $("body").addClass("ios");
+  }
+
+  //Worksのリンクを有効化
+  $(".works-url").on("click", "a", function (e) {
+    e.stopPropagation();
+  });
+
+  //ページ内スクロール
+  var $nav = $(".gnav");
+  var navHeight = $nav.outerHeight();
+
+  $('a[href^="#"]').on("click", function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top - navHeight;
+    $("html, body").animate(
+      {
+        scrollTop: position,
+      },
+      380,
+      "swing"
+    );
+    return false;
+  });
+
+  //ページトップ
+  $("#js-page-top").on("click", function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      300
+    );
+    return false;
+  });
+
+   //モーダルウィンドウ
+  $('.js-modal-open').each(function(){
+      $(this).on('click',function(){
+          var target = $(this).data('target');
+          var modal = document.getElementById(target);
+          $(modal).fadeIn();
+          return false;
+      });
+  });
+  $('.js-modal-close').on('click',function(){
+      $('.js-modal').fadeOut();
+      return false;
+  });
+
+});
